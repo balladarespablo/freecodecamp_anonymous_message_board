c1ef9213c5ef0e50f65d6018cc7da8913e29ebd3
Pablo Balladares <balladarespablo@gmail.com>
2025-09-03 17:07:52 +0000

    add: subo proyecto terminado

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const routerThreads = require("../controllers/thread.controller.js");
+const routerReplies = require("../controllers/reply.controller.js");
+const express = require("express");
+const router = express.Router();
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+  app.use("/", routerThreads(router));
 
+  app.use("/", routerReplies(router));
 };
--- server.js
+++ server.js
@@ -1,65 +1,76 @@
-'use strict';
-require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
-
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const connectDB = require("./db.js");
+const apiRoutes = require("./routes/api.js");
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const runner = require("./test-runner");
 
 const app = express();
+app.use(
+  helmet({
+    xFrameOptions: { action: "sameorigin" },
+    xDnsPrefetchControl: { allow: false },
+    referrerPolicy: { policy: "same-origin" },
+  })
+);
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//Sample front-end
-app.route('/b/:board/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/board.html');
-  });
-app.route('/b/:board/:threadid')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/thread.html');
-  });
+async function start() {
+  try {
+    await connectDB.connect();
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+    //Sample front-end
+    app.route("/b/:board/").get(function (req, res) {
+      res.sendFile(process.cwd() + "/views/board.html");
+    });
+    app.route("/b/:board/:threadid").get(function (req, res) {
+      res.sendFile(process.cwd() + "/views/thread.html");
+    });
 
-//For FCC testing purposes
-fccTestingRoutes(app);
+    //Index page (static HTML)
+    app.route("/").get(function (req, res) {
+      res.sendFile(process.cwd() + "/views/index.html");
+    });
 
-//Routing for API 
-apiRoutes(app);
+    //For FCC testing purposes
+    fccTestingRoutes(app);
 
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
+    //Routing for API
+    apiRoutes(app);
 
-//Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        console.log('Tests are not valid:');
-        console.error(e);
+    //404 Not Found Middleware
+    app.use(function (req, res, next) {
+      res.status(404).type("text").send("Not Found");
+    });
+
+    //Start our server and tests!
+    const listener = app.listen(process.env.PORT || 3000, function () {
+      console.log("Your app is listening on port " + listener.address().port);
+      if (process.env.NODE_ENV === "test") {
+        console.log("Running Tests...");
+        setTimeout(function () {
+          try {
+            runner.run();
+          } catch (e) {
+            console.log("Tests are not valid:");
+            console.error(e);
+          }
+        }, 1500);
       }
-    }, 1500);
+    });
+  } catch (error) {
+    console.log("Error: error al conectar a la base de datos" + error.message);
   }
-});
+}
+
+start();
 
 module.exports = app; //for testing
